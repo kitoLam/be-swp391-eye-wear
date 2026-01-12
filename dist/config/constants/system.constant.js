24163e6a9d255f1d15935b394467726e396ffa9e
Minh Lam <qm02192803@gmail.com>
2026-01-12 18:01:25 +0700

    add

--- dist/config/constants/system.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.systemConstant = void 0;
-exports.systemConstant = {
-    prefixPathAdmin: "admin",
-    prefixPathClient: ""
-};
