8ba67b485858efe367110888768b0a3df3825c76
Thangwibu <lameojicoemail@gmail.com>
2026-01-15 18:02:35 +0700

    #11 - create product controller and route

--- src/config/constants/response-messages/product.constant.ts
+++ src/config/constants/response-messages/product.constant.ts
@@ -6,6 +6,7 @@ export const ProductMessage = {
         delete: 'Xóa sản phẩm' + successSuffix,
         getList: 'Lấy danh sách sản phẩm' + successSuffix,
         getDetail: 'Láy sản phẩm' + successSuffix,
+        search: 'Tìm kiếm sản phẩm' + successSuffix,
     },
     error: {},
 };
