9a40d91d5b53af4ecdf6cc173c2e2adb290858ba
Minh Lam <qm02192803@gmail.com>
2026-01-27 10:06:52 +0700

    add

--- src/routes/admin/invoice.route.ts
+++ src/routes/admin/invoice.route.ts
@@ -3,15 +3,14 @@ import { validateParams, validateQuery } from "../../middlewares/share/validator
 import { InvoiceListQuerySchema } from "../../types/invoice/invoice.query";
 import invoiceController from "../../controllers/admin/invoice.controller";
 import { authenticateMiddleware } from "../../middlewares/admin/auth.middleware";
-import { InvoiceVerifyParams } from "../../types/invoice/invoice.params";
 import { ObjectIdSchema } from "../../types/common/objectId";
 const router = Router();
 router.use(authenticateMiddleware);
 // api lấy danh sách hóa đơn
 router.get('/', validateQuery(InvoiceListQuerySchema), invoiceController.getListInvoice);
 
-router.patch('/:id/verify/approve', validateParams(ObjectIdSchema), invoiceController.approveInvoice);
+router.patch('/:id/status/approve', validateParams(ObjectIdSchema), invoiceController.approveInvoice);
 
-router.patch('/:id/verify/reject', validateParams(ObjectIdSchema), invoiceController.rejectInvoice);
+router.patch('/:id/status/reject', validateParams(ObjectIdSchema), invoiceController.rejectInvoice);
 
 export default router;
