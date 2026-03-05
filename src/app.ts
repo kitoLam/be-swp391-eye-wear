2aafe2031e72164e66f246cf71c9cbfefbd9ae63
Minh Lam <qm02192803@gmail.com>
2026-03-05 14:33:50 +0700

    re-run

--- src/app.ts
+++ src/app.ts
@@ -89,6 +89,6 @@ app.use(
 app.use((req, res) => {
     res.status(404).json({ error: 'Route is not found' });
 });
-// add new
+// add new cmt
 app.use(errorMiddleware);
 export { app, httpServer };
